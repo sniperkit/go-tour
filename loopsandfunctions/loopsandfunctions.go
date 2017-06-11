2c406dbc09ac5a27bc789ab0dcd12c166e1d9bbb
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-06-11 22:54:52 +0100

    sqrt -> loopsandfunctions

--- loopsandfunctions/loopsandfunctions.go
+++ loopsandfunctions/loopsandfunctions.go
@@ -1,4 +1,4 @@
-package sqrt
+package loopsandfunctions
 
 import "math"
 
--- loopsandfunctions/loopsandfunctions_test.go
+++ loopsandfunctions/loopsandfunctions_test.go
@@ -1,17 +1,17 @@
-package sqrt_test
+package loopsandfunctions_test
 
 import (
 	"fmt"
 	"math"
 	"testing"
 
-	"github.com/sahilm/go-tour/sqrt"
+	"github.com/sahilm/go-tour/loopsandfunctions"
 )
 
 func TestSqrt(t *testing.T) {
 	cases := []float64{1, 2, 3.3, 4, 1000, 1024}
 	for _, c := range cases {
-		got, iterations := sqrt.Sqrt(c)
+		got, iterations := loopsandfunctions.Sqrt(c)
 		want := math.Sqrt(c)
 		if got != want {
 			t.Errorf("Sqrt(%f) == %f, want %f", c, got, want)
