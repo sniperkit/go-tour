653e740c48beb6453a23c9fbefa7b4430de3700a
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-06-11 22:31:30 +0100

    exercise1 -> sqrt

--- sqrt/sqrt_test.go
+++ sqrt/sqrt_test.go
@@ -1,17 +1,17 @@
-package exercise1_test
+package sqrt_test
 
 import (
 	"fmt"
 	"math"
 	"testing"
 
-	"github.com/sahilm/go-tour/exercise1"
+	"github.com/sahilm/go-tour/sqrt"
 )
 
 func TestSqrt(t *testing.T) {
 	cases := []float64{1, 2, 3.3, 4, 1000, 1024}
 	for _, c := range cases {
-		got, iterations := exercise1.Sqrt(c)
+		got, iterations := sqrt.Sqrt(c)
 		want := math.Sqrt(c)
 		if got != want {
 			t.Errorf("Sqrt(%f) == %f, want %f", c, got, want)
