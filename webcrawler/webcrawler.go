6c617c7155a5464303192cf4b83be5d458be545e
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-06-29 22:01:09 +0100

    Document stuff and I guess my code is fairly original now :).

--- webcrawler/webcrawler.go
+++ webcrawler/webcrawler.go
@@ -1,4 +1,3 @@
-// Adapted from the official solution with the following copyright: Copyright 2012 The Go Authors.  All rights reserved.
 package webcrawler
 
 import (
@@ -72,16 +71,22 @@ func (c *Crawler) Crawl() ([]*Page, []error) {
 var sentinel = struct{}{}
 
 func crawl(url string, depth int, fetcher Fetcher, visited *concurrent.Map, pages *concurrent.Slice, errors *concurrent.Slice) {
+	// We've reached max depth. Time to bail.
 	if depth <= 0 {
 		return
 	}
 
+	// The intention is to claim the url for this goroutine.
+	// If someone else has already claimed it, we'll just exit.
 	_, ok := visited.PutIfAbsent(url, sentinel)
 	if !ok {
 		return
 	}
 
 	page, err := fetcher.Fetch(url)
+
+	// Indicate that the url has been crawled. Others will notice this
+	// when they they try a PutIfAbsent.
 	visited.Put(url, sentinel)
 
 	if err != nil {
@@ -92,6 +97,7 @@ func crawl(url string, depth int, fetcher Fetcher, visited *concurrent.Map, page
 	pages.Append(page)
 	done := make(chan bool)
 
+	// Spawn as many goroutines as links on the page.
 	for _, url := range page.Links {
 		go func(url string) {
 			crawl(url, depth-1, fetcher, visited, pages, errors)
@@ -99,7 +105,10 @@ func crawl(url string, depth int, fetcher Fetcher, visited *concurrent.Map, page
 		}(url)
 	}
 
+	// Wait for all of them to finish.
 	for range page.Links {
 		<-done
 	}
+
+	// We're done crawling this page. On to the next!
 }
