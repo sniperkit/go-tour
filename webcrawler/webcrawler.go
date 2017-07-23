d949a5ce6b4ca722bb3fd8727bdf9720d58b5e1e
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-07-23 14:18:24 +0100

    Cut down on the whitespace.

--- webcrawler/webcrawler.go
+++ webcrawler/webcrawler.go
@@ -74,24 +74,19 @@ func crawl(url string, depth int, fetcher Fetcher, visited *concurrent.Map, page
 	if depth <= 0 {
 		return
 	}
-
 	// The intention is to claim the url for this goroutine.
 	// If someone else has already claimed it, we'll just exit.
 	_, ok := visited.PutIfAbsent(url, sentinel)
 	if !ok {
 		return
 	}
-
 	page, err := fetcher.Fetch(url)
-
 	if err != nil {
 		errors.Append(err)
 		return
 	}
-
 	pages.Append(page)
 	done := make(chan bool)
-
 	// Spawn as many goroutines as links on the page.
 	for _, url := range page.Links {
 		go func(url string) {
@@ -99,11 +94,9 @@ func crawl(url string, depth int, fetcher Fetcher, visited *concurrent.Map, page
 			done <- true
 		}(url)
 	}
-
 	// Wait for all of them to finish.
 	for range page.Links {
 		<-done
 	}
-
 	// We're done crawling this page. On to the next!
 }
